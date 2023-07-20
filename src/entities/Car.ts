0aaf39a24419b8904705b91378709d952e68f007
pawelLinek <pawellinek3D@gmail.com>
2023-07-20 21:49:53 +0200

    changed car moving logic

--- src/entities/Car.ts
+++ src/entities/Car.ts
@@ -6,36 +6,74 @@ export class Car implements CarType {
   y: number;
   width: number;
   height: number;
+  speed: number;
+  acceleration: number;
+  maxSpeed: number;
+  friction: number;
+  angle: number;
   controls: InputController;
   constructor(x: number, y: number, width: number, height: number) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.angle = 0;
+
     this.controls = new InputController();
   }
-  draw(ctx: CanvasRenderingContext2D) {
-    ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
-    ctx.fill();
-  }
-  update() {
+  private upDownControlls() {
     if (this.controls.forward) {
-      this.y -= 2;
+      this.speed += this.acceleration;
     }
     if (this.controls.reverse) {
-      this.y += 2;
+      this.speed -= this.acceleration;
     }
-    if (this.controls.left) {
-      this.x -= 2;
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
     }
-    if (this.controls.right) {
-      this.x += 2;
+    if (this.speed < -this.maxSpeed / 1.7) {
+      this.speed = -this.maxSpeed / 1.7;
     }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+  }
+  update() {
+    this.upDownControlls();
+
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+  }
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+    ctx.beginPath();
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.fill();
+    ctx.restore();
   }
 }
