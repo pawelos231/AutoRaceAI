43511c527083cbb3bb567cff7d26e81e07dc1277
pawelLinek <pawellinek3D@gmail.com>
2023-07-26 19:53:14 +0200

    started implementation of Neural network

---
+++ src/network/index.ts
@@ -0,0 +1,19 @@
+import { Level } from "./level";
+
+class NeuralNetwork {
+  levels: Level[];
+  constructor(neuronCounts: number[]) {
+    this.levels = [];
+    for (let i = 0; i < neuronCounts.length - 1; i++) {
+      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+    }
+  }
+
+  static feedForward(givenInputs: any, network: NeuralNetwork): number[] {
+    let outputs = Level.feedForward(givenInputs, network.levels[0]);
+    for (let i = 1; i < network.levels.length; i++) {
+      outputs = Level.feedForward(outputs, network.levels[i]);
+    }
+    return outputs;
+  }
+}
